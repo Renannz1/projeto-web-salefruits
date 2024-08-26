84611cb6bb45d51ffa70e8f76b780ec3087c82d4
Renan <renanmagalhaesreis@gmail.com>
2024-08-26 01:16:33 -0300

    adicionado o app produto + models de produto

---
+++ produto/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Produto(models.Model):
+    nome = models.CharField(max_length=100)
+    descricao = models.TextField()
+    preco = models.DecimalField(max_digits=10, decimal_places=2)
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE, related_name='produtos')
+    postado_em = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return self.nome
