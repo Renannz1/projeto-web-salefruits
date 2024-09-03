01c849bbe0efb927f7a0108c5b70d7a44a06a90f
Jean Barbosa <Jeanb.santos404@gmail.com>
2024-09-03 20:42:33 -0300

    adição da pagina edita perfil e excluir perfil, alterações na view usuarios

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -4,11 +4,9 @@ from .views import custom_logout, register, login_view
 
 
 urlpatterns = [
-    path('editar/<int:usuario_id>/', views.editar_usuario, name='editar_usuario'),
-    path('excluir/<int:usuario_id>/', views.excluir_usuario, name='excluir_usuario'),
-
+    path('editar/<int:usuario_id>/', views.editar_usuario, name='editar'),
+    path('excluir/<int:id>/', views.excluir_usuario, name='excluir'),
     path('perfil/', views.perfil_usuario, name='perfil_usuario'),
-
     path('register/', register, name='register'),
     path('login/', login_view, name='login'),
     path('logout/', custom_logout, name='logout'),
--- usuarios/views.py
+++ usuarios/views.py
@@ -13,6 +13,7 @@ from django.contrib.auth.models import User
 
 
 # ------- metodo para editar usuario -------
+@login_required
 def editar_usuario(request, usuario_id):
     # Busca o objeto Usuario correspondente ao usuario_id no banco de dados.
     # Se não encontrar, retorna uma página 404 (não encontrado).
@@ -35,7 +36,7 @@ def editar_usuario(request, usuario_id):
             # Se a requisição não for POST, cria uma instância do formulário com os dados atuais do perfil
             form = UsuarioForm(instance=usuario)
         # Renderiza a página de edição de usuário com o formulário (vazio ou preenchido)
-        return render(request, 'login/editar_usuario.html', {'form': form})
+        return render(request, 'login/editar.html', {'form': form})
     else:
         # Se o usuário logado não tiver permissão, lança uma exceção de permissão negada
         raise PermissionDenied
@@ -44,50 +45,39 @@ def editar_usuario(request, usuario_id):
 
 
 # ------- metodo para excluir usuario -------
-def excluir_usuario(request, usuario_id):
-    # Tenta obter o usuário pelo id, ou retorna um erro 404 se não encontrado
-    usuario = get_object_or_404(Usuario, id=usuario_id)
-    
-    # Verifica se o usuário logado tem permissão para excluir
-    if request.user.is_staff or request.user == usuario.user:
-        # Verifica se o método da requisição é POST (confirmação de exclusão)
-        if request.method == 'POST':
-            # Exclui o usuário do banco de dados
+@login_required
+def excluir_usuario(request, id):
+    if request.method == 'POST':
+        # Obtém o objeto User correspondente ao id fornecido
+        user = get_object_or_404(User, id=id)
+        
+        # Obtém o objeto Usuario associado ao User
+        try:
+            usuario = Usuario.objects.get(user=user)
             usuario.delete()
-            # Redireciona para a página de listagem de usuários após a exclusão
-            return redirect('listar_mural')
-        # Renderiza a página de confirmação de exclusão com o usuário
-        return render(request, 'login/confirmar_exclusao.html', {'usuario': usuario})
+        except Usuario.DoesNotExist:
+            # Se o objeto Usuario não for encontrado, você pode adicionar uma mensagem de erro ou logar o evento
+            pass
+        
+        # Exclua o User
+        user.delete()
+    
+        return redirect('login')
     else:
-        # Se o usuário logado não tem permissão, levanta uma exceção de permissão negada
-        raise PermissionDenied
-
+        # Exiba um formulário de confirmação ou uma página apropriada
+        return render(request, 'login/excluir.html', {'user': get_object_or_404(User, id=id)})
 
 
 
 # ------- metodo para exibir o perfil do usuario -------
+
 @login_required
 def perfil_usuario(request):
     # Obtém o perfil do usuário logado
-    usuario = request.user.usuario
-
-    # Verifica se o método da requisição é POST (submissão de formulário)
-    if request.method == 'POST':
-        # Cria uma instância do formulário com os dados enviados e preenche com o perfil do usuário
-        form = UsuarioForm(request.POST, instance=usuario)
-        # Verifica se o formulário é válido
-        if form.is_valid():
-            # Salva as alterações no perfil do usuário
-            form.save()
-            # Redireciona para a página de listagem de usuários após salvar
-            return redirect('listar_mural')
-    else:
-        # Se a requisição não for POST, cria uma instância do formulário com os dados atuais do perfil
-        form = UsuarioForm(instance=usuario)
-
-    # Renderiza o template de perfil do usuário com o formulário (vazio ou preenchido)
-    return render(request, 'login/perfil_usuario.html', {'form': form})
-
+    usuario = get_object_or_404(Usuario, user=request.user)
+    
+    # Renderiza o template de perfil do usuário com os detalhes
+    return render(request, 'login/perfil.html', {'usuario': usuario})
 
 
 
@@ -133,6 +123,7 @@ def register(request):
 
 
 # ------- metodo para login do usuario -------
+
 def login_view(request):
     # Verifica se o metódo da requisição é do tipo POST
     if request.method == "POST":
