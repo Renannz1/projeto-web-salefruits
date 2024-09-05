58eae626b16088c9dab2227f3a784481c7666f9b
Jean Barbosa <Jeanb.santos404@gmail.com>
2024-09-05 08:26:01 -0300

    alteração

--- salefruits/settings.py
+++ salefruits/settings.py
@@ -53,7 +53,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    
 ]
+
+
 
 ROOT_URLCONF = 'salefruits.urls'
 
